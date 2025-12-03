e13995678241d37cc63ded74bd75c00b0e806eff
Burak Topcu <but001@edu.zealand.dk>
2025-12-03 11:49:49 +0100

    Event

--- Hilerrøds sejle/Model/Events.cs
+++ Hilerrøds sejle/Model/Events.cs
@@ -2,6 +2,16 @@
 {
     public class Events
     {
-        
+        public string Navn { get; set; }
+        public DateTime Tidspunkt { get; }
+
+
+        public Events(string navn, DateTime tidspunkt)
+
+        {
+            Navn = navn;
+            Tidspunkt = tidspunkt;
+
+        }
     }
 }
