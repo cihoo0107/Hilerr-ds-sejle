be50d56d0d95df7c9b4f7f5a7ecb22ee27a1f6ce
Cihan Tokmak <cihoo0107@gmail.com>
2025-12-03 14:23:55 +0100

    update

--- Hilerrøds sejle/Pages/Både/Index.cshtml.cs
+++ Hilerrøds sejle/Pages/Både/Index.cshtml.cs
@@ -9,7 +9,8 @@ namespace Hilerrøds_sejle.Pages.Både
         public List<Båd> Både { get; set; } = new List<Båd>
         {
            new Båd("Motorbåd","X20","4120",10,4,"2005","Lambo"),
-           new Båd("Sejlbåd", "Q35","3115",8,3,"2003","The Quruce")
+           new Båd("Sejlbåd", "Q35","3115",8,3,"2003","The Quruce"),
+           new Båd("Skoda", "60","3112",4,1,"2022","Enyaq")
         };
 
         public void OnGet()
