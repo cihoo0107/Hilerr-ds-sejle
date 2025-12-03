c33ff6da17c206dff5eff92a340744d6fd1b259e
emrekaydk <emrekaydk@gmail.com>
2025-12-03 11:31:41 +0100

    Medlem -> medlemmer

--- Hilerrøds sejle/Pages/Medlemmer/Medlemmer.cs
+++ Hilerrøds sejle/Pages/Medlemmer/Medlemmer.cs
@@ -1,7 +1,7 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
-namespace Hilerrøds_sejle.Pages.Medlem
+namespace Hilerrøds_sejle.Pages.Medlemmer
 {
     public class IndexModel : PageModel
     {
