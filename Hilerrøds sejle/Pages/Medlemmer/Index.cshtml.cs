8108ec8769fca80b63e8800666653d7713f99f26
emrekaydk <emrekaydk@gmail.com>
2025-12-05 23:12:46 +0100

    Edit created

--- Hilerrøds sejle/Pages/Medlemmer/Index.cshtml.cs
+++ Hilerrøds sejle/Pages/Medlemmer/Index.cshtml.cs
@@ -3,20 +3,20 @@ using Hilerrøds_sejle.Model;
 using System.Collections.Generic;
 using Microsoft.AspNetCore.Mvc;
 
-namespace Hilerrøds_sejle.Pages.Medlemmer
-{
-    public class IndexModel : PageModel
-    {
-        public List<Medlem> Medlemmer { get; set; } = new List<Medlem>()
-        {
-            new Medlem("Niels","Niels@mail.com",001,Medlemsrolle.Formand),
-            new Medlem("Peter","Peterersej@mail.com",007,Medlemsrolle.Medlem),
-            new Medlem("Maja","Maja@mail.com",002,Medlemsrolle.Næstforkvinde),
-        };
+//namespace Hilerrøds_sejle.Pages.Medlemmer
+//{
+//    public class IndexModel : PageModel
+//    {
+//        public List<Medlem> Medlemmer { get; set; } = new List<Medlem>()
+//        {
+//            new Medlem("Niels","Niels@mail.com",001,Medlemsrolle.Formand),
+//            new Medlem("Peter","Peterersej@mail.com",007,Medlemsrolle.Medlem),
+//            new Medlem("Maja","Maja@mail.com",002,Medlemsrolle.Næstforkvinde),
+//        };
 
-        public void OnGet()
-        {
-        }
+//        public void OnGet()
+//        {
+//        }
 
-    }
-}
+//    }
+//}
---
+++ Hilerrøds sejle/Pages/Medlemmer/Opret.cshtml.cs
@@ -0,0 +1,36 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Hilerrøds_sejle.Model;
+using Hilerrøds_sejle.Service;
+
+namespace Hilerrøds_sejle.Pages.Medlemmer
+{
+    public class OpretModel : PageModel
+    {
+        private readonly IMedlemRepository _repo;
+
+        public OpretModel(IMedlemRepository repo)
+        {
+            _repo = repo;
+            NyMedlem = new Medlem(); // vigtigt til form-binding
+        }
+
+        [BindProperty]
+        public Medlem NyMedlem { get; set; }
+
+        public void OnGet()
+        {
+        }
+
+        public IActionResult OnPost()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            _repo.Add(NyMedlem);
+            return RedirectToPage("Index");
+        }
+    }
+}
