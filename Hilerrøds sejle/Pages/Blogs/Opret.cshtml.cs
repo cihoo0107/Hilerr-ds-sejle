647138af7f814206821afe8b5bff74fbb57c5863
Ayman Messaoudi <aym002@edu.zealand.dk>
2025-12-11 11:47:32 +0100

    update

---
+++ Hilerrøds sejle/Pages/Blogs/Opret.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Hilerrøds_sejle.Pages.Blogs
+{
+    public class OpretModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
