bcefcf4be47a7cabb080ce1ef02e3ece673d7520
agersk0v <agerskov40@gmail.com>
2025-12-11 11:48:29 +0100

    booking add

--- Hilerrøds sejle/Pages/Bookings/Opret.cshtml.cs
+++ Hilerrøds sejle/Pages/Bookings/Opret.cshtml.cs
@@ -7,31 +7,43 @@ namespace Hilerrøds_sejle.Pages.Bookings
 {
     public class OpretModel : PageModel
     {
-        private IBookingService _bookingService;
+        private readonly IBookingService _bookingService;
+        private readonly IMedlemRepository _medlemService;
 
-        public OpretModel(IBookingService bookingService)
+        public List<Medlem> Medlemmer { get; private set; } = new();
+
+        [BindProperty]
+        public int SelectedMedlemId { get; set; }
+
+        public OpretModel(IBookingService bookingService, IMedlemRepository medlemService)
         {
             _bookingService = bookingService;
+            _medlemService = medlemService;
         }
-
 
         public void OnGet()
         {
+            Medlemmer = _medlemService.GetAll();
         }
 
-        public IActionResult OnPost() {
+        public IActionResult OnPost()
+        {
             if (!ModelState.IsValid)
+                return Page();
+
+            var medlem = _medlemService.GetById(SelectedMedlemId);
+
+            if (medlem == null)
             {
+                ModelState.AddModelError("", "Medlem findes ikke.");
                 return Page();
             }
 
-            _bookingService.Add(
-                new Booking(
-                    new Båd("Motorbåd", "X20", "4120", 10, 4, "2005", "Lambo"),
-                    new Medlem("Niels", "Niels@mail.com", 001, Medlemsrolle.Formand)
-                )
-            );
- 
+            var båd = new Båd("Motorbåd", "X20", "4120", 10, 4, "2005", "Lambo");
+
+            var booking = new Booking(båd, medlem);
+            _bookingService.Add(booking);
+
             return RedirectToPage("Index");
         }
     }
