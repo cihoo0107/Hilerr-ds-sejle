654ddc4d4179c8c0d5e60a026e4c289556f201a0
emrekaydk <emrekaydk@gmail.com>
2025-12-12 11:12:28 +0100

    Rediger.cshtml.cs færdig

---
+++ Hilerrøds sejle/Pages/Medlemmer/Rediger.cshtml.cs
@@ -0,0 +1,47 @@
+using Hilerrøds_sejle.Model;
+using Hilerrøds_sejle.Service;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Hilerrøds_sejle.Pages.Medlemmer
+{
+    public class RedigerModel : PageModel
+    {
+
+        private readonly IMedlemService _repo;
+
+        public RedigerModel(IMedlemService repo)
+        {
+            _repo = repo;
+        }
+
+        [BindProperty]
+        public Medlem MedlemToEdit { get; set; }
+
+        public IActionResult OnGet(int id)
+        {
+            MedlemToEdit = _repo.GetById(id);
+            if (MedlemToEdit == null) return NotFound();
+            return Page();
+        }
+
+        public IActionResult OnPost()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            var existing = _repo.GetById(MedlemToEdit.Id);
+            if (existing == null) return NotFound();
+
+            existing.Navn = MedlemToEdit.Navn;  
+            existing.Email = MedlemToEdit.Email;
+            existing.Rolle = MedlemToEdit.Rolle;
+
+            _repo.Update(existing);
+
+            return RedirectToPage("Index");
+        }
+    }
+}
