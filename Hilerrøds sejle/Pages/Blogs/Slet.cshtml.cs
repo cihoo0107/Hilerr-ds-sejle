fa04733efc10551439ef445e31fc3c180f89921a
Ayman Messaoudi <aym002@edu.zealand.dk>
2025-12-12 11:03:56 +0100

    update

---
+++ Hilerrøds sejle/Pages/Blogs/Slet.cshtml.cs
@@ -0,0 +1,34 @@
+using Hilerrøds_sejle.Model;
+using Hilerrøds_sejle.Service;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.Diagnostics.Metrics;
+
+namespace Hilerrøds_sejle.Pages.Blogs
+{
+    public class SletModel : PageModel
+    {
+        private readonly IBlogService _blogService;
+
+        [BindProperty]
+        public Blog BlogToDelete { get; set; }
+
+        public SletModel(IBlogService blogService)
+        {
+            _blogService = blogService;
+        }
+
+        public IActionResult OnGet(string id)
+        {
+            BlogToDelete = _blogService.GetAll().FirstOrDefault(blog => blog.Id == id);
+            if (BlogToDelete == null)
+            {
+                return RedirectToPage("/Blog/Index");
+            }
+
+            return Page();
+        }
+    }
+}
+
+            
