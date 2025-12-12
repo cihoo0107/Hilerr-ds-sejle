651a29432dfa5dd2702e41e85d1d95b75cee4ef3
emrekaydk <emrekaydk@gmail.com>
2025-12-12 11:37:28 +0100

    Rediger

---
+++ Hilerrøds sejle/Pages/Bookings/Rediger.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Hilerrøds_sejle.Pages.Bookings
+{
+    public class RedigerModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
---
+++ Hilerrøds sejle/Pages/Bookings/Slet.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Hilerrøds_sejle.Pages.Bookings
+{
+    public class SletModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
