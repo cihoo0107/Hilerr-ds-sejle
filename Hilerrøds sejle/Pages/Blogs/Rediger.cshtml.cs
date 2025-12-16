c5eb89febbf87907042d0819d94d10384cc8a278
Ayman Messaoudi <aym002@edu.zealand.dk>
2025-12-16 11:29:12 +0100

    new

--- Hilerrøds sejle/Pages/Blogs/Rediger.cshtml.cs
+++ Hilerrøds sejle/Pages/Blogs/Rediger.cshtml.cs
@@ -1,12 +1,37 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Hilerrøds_sejle.Model;
+using Hilerrøds_sejle.Service;
 
 namespace Hilerrøds_sejle.Pages.Blogs
 {
     public class RedigerModel : PageModel
     {
-        public void OnGet()
+        private readonly IBlogService _blogService;
+
+        [BindProperty]
+        public Blog BlogToEdit { get; set; }
+        public RedigerModel(IBlogService blogService)
         {
+            _blogService = blogService;
+        }
+        public IActionResult OnGet(string id)
+        {
+            BlogToEdit = _blogService.GetAll().FirstOrDefault(blog => blog.Id == id);
+            if (BlogToEdit == null)
+            {
+                return RedirectToPage("/Blog/Index");
+            }
+            return Page();
+        }
+        public IActionResult OnPost()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+            _blogService.Update(BlogToEdit);
+            return RedirectToPage("/Blog/Index");
         }
     }
 }
